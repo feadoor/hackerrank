9b2b6d454ac322f2b851a36f09abe8c955a3f5b8
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2018-02-05 07:46:18 +0000

    Start the implementation problems

---
+++ practice/algorithms/implementation/grading_students.py
@@ -0,0 +1,13 @@
+def rounded_grade(grade):
+    if grade < 38 or grade % 5 < 3:
+        return grade
+    else:
+        return grade + 5 - grade % 5
+
+def main():
+    num_grades = input()
+    for _ in xrange(num_grades):
+        print rounded_grade(input())
+
+if __name__ == "__main__":
+    main()
